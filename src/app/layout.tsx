88bdf1c52e328895cce01fe2816e7a101c4d3526
Leonidas Vargas Araya <leva0212@gmail.com>
2026-05-27 10:40:02 -0600

    Agregado logo, update titulo pagina

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,95 @@ import { VersionCheck } from "@/app/version-check";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+
+import { Providers } from "@/app/providers";
+import { Toaster } from "sonner";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: {
+    default: "Agiliza",
+    template: "%s | Agiliza",
+  },
+
+  description: "Plataforma logística multiempresa",
+
+  manifest: "/manifest.json",
+
+  icons: {
+    icon: [
+      {
+        url: "/icons/icon-192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        url: "/icons/icon-512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+
+    apple: [
+      {
+        url: "/icons/icon-192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+    ],
+
+    shortcut: "/icons/icon-192.png",
+  },
+};
+
+type RootLayoutProps = Readonly<{
+  children: React.ReactNode;
+}>;
+
+export default function RootLayout({
+  children,
+}: RootLayoutProps) {
+  return (
+    <html
+      lang="es"
+      className={`
+        ${geistSans.variable}
+        ${geistMono.variable}
+        h-full
+        antialiased
+      `}
+    >
+      <body className="min-h-screen bg-white text-black">
+        <VersionCheck />
+
+        <Providers>
+          {children}
+        </Providers>
+
+        <Toaster
+          richColors
+          position="top-center"
+          duration={2000}
+          closeButton
+        />
+      </body>
+    </html>
+  );
+}
+
+/*import { VersionCheck } from "@/app/version-check";
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
 //import "@/app/globals.css";
 
 import { Providers } from "@/app/providers";
@@ -45,3 +134,4 @@ export default function RootLayout({ children }: RootLayoutProps) {
     </html>
   );
 }
+*/
